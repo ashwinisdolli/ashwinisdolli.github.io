afe7566c4bdae3d027165e12f5461615161403d5
ashwinisdolli <97789581+ashwinisdolli@users.noreply.github.com>
2022-07-07 13:14:56 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,17 @@
+function circlearea(){
+var ab= document.getElementById('a1').value;
+ var bc= document.getElementById('a2').value;
+ var cd= document.getElementById('a3').value;
+ var area=3.14*r^2;
+ document.getElementById('a2').value= area;
+ console.log(area)
+}
+
+function circleperimeter(){
+var xy= document.getElementById('a1').value;
+ var yz= document.getElementById('a2').value;
+ var za= document.getElementById('a3').value;
+ var perimeter=2*3.14*r;
+ document.getElementById('a3').value= perimeter;
+ console.log(perimeter)
+}
