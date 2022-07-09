2d2bcf0eec0804e7b1a3568cf6ed5bc82c9c988a
ashwinisdolli <97789581+ashwinisdolli@users.noreply.github.com>
2022-07-09 16:33:27 +0530

    compound interest

---
+++ ci.js
@@ -0,0 +1,9 @@
+function compound(){
+    var a1=document.getElementById('p').value;
+    var a2=document.getElementById('r').value;
+    var a3=document.getElementById('n').value;
+    var a4=document.getElementById('t').value;
+    var ans=parseInt(a1)*(1+parseInt(a2)/parseInt(a3))^(parseInt(n)+parseInt(t));
+    document.getElementById('t1').value=ans;
+     
+}
